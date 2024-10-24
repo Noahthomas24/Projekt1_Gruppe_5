57345995f26147870601f9fa7102e7cdb42d644f
juliusbay <juax0001@stud.kea.dk>
2024-10-24 15:41:47 +0200

    Bruger, kunde - fjernelse af telefonnummer

--- src/Bruger.java
+++ src/Bruger.java
@@ -1,6 +1,3 @@
-import java.time.LocalTime;
-import java.util.ArrayList;
-
 public abstract class Bruger {
     String name;
 
@@ -8,12 +5,10 @@ public abstract class Bruger {
 
 class Customer extends Bruger{
     Time t;
-    int phone;
     private static int i= 0;
     static int kundeID=0;
     public Customer(String name, Time t) {
         this.name = name;
-        this.phone = phone;
         this.t = t;
         i++;
         kundeID = i;
