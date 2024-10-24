f03b25063274bda39ffdb6e39c28f125cf8f1913
Noahthomas24 <noth0001@stud.kea.dk>
2024-10-24 16:57:49 +0200

    Datohåndtering forbedret.

--- src/WeeklyCalendar.java
+++ src/WeeklyCalendar.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WeeklyCalendar {
     int i;
+    LocalDate dateDatoValg;
     Scanner scanner = new Scanner(System.in);
     ArrayList<Time> generedeTider = new ArrayList();
     ArrayList<Time> ledigeTider = new ArrayList();
@@ -19,7 +21,7 @@ public class WeeklyCalendar {
         //System.out.println("\nWeekly Calendar:");
 
     public void genTid() {
-        for (i = 0; i < 7; i++) {
+        for (i = 0; i < 5; i++) {
             LocalDate currentDate = startDate.plusDays(i);
             DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
             if (dayOfWeek!=DayOfWeek.SATURDAY && dayOfWeek!=DayOfWeek.SUNDAY) {
@@ -40,7 +42,9 @@ public class WeeklyCalendar {
 
     public void bookTid(){//Modtager en scanner, hvor brugeren kan angive et tal til at vælge en ledig tid ud fra index nummeret.
         System.out.println("Indtast nummer for den tid du gerne vil vælge.");
-        Time valgTid = ledigeTider.get(scanner.nextInt()-1);
+        List<Time> tider = getTiderForDato(dateDatoValg);
+        int valg = scanner.nextInt()-1;
+        Time valgTid = tider.get(valg);
         System.out.println("Du har valgt tiden: "+valgTid);
         System.out.println("Indtast dit navn.");
         scanner.nextLine();
@@ -52,11 +56,64 @@ public class WeeklyCalendar {
 
     public void visTider() {// Viser alle ledige tider i listen ledigeTider
         int i = 0;
-        for (Time s:ledigeTider){
+        List<LocalDate> dates = getNextFiveDays();
+        System.out.println(dates);
+        int valgtDato = scanner.nextInt();
+        dateDatoValg = dates.get(valgtDato-1);
+        printTiderForDato(dateDatoValg);
+        /*switch (valgtDato){
+            case 1:
+                printTiderForDato(dates.get(valgtDato-1));
+                break;
+            case 2:
+                printTiderForDato(dates.get(valgtDato-1));
+                break;
+            case 3:
+                printTiderForDato(dates.get(valgtDato-1));
+                break;
+            case 4:
+                printTiderForDato(dates.get(valgtDato-1));
+                break;
+            case 5:
+                printTiderForDato(dates.get(valgtDato-1));
+                break;
+            default:
+                break;
+        }*/
+       /* for (Time s:ledigeTider){
             i++;
             System.out.println(i+ ": "+s);
+        }*/
+    }
+
+    private void printTiderForDato(LocalDate date) {
+        for (int i = 0; i<getTiderForDato(date).size();i++){
+            if (getTiderForDato(date).get(i).dato == date){
+                System.out.println(i+1+": "+getTiderForDato(date).get(i));
+            }
         }
     }
+    private List<Time> getTiderForDato(LocalDate date) {
+        List<Time> tider = new ArrayList<>();
+        for (int i = 0; i<ledigeTider.size();i++){
+            if (ledigeTider.get(i).dato == date){
+                tider.add(ledigeTider.get(i));
+            }
+        }
+        return tider;
+    }
+
+    private List<LocalDate> getNextFiveDays() {
+        List<LocalDate> dates = new ArrayList<>();
+        for (int i=0; i<ledigeTider.size();i++){
+            Time tid = ledigeTider.get(i);
+            if (!dates.contains(tid.dato)){
+                dates.add(tid.dato);
+            }
+        }
+        return dates;
+    }
+
     public void annullerTid(){
         int i = 0;
         for (Customer s:booketTider){
