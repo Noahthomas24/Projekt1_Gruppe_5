e8da0cdafe0138b53c68fb3332474370d2948f75
Noahthomas24 <noth0001@stud.kea.dk>
2024-10-28 13:02:29 +0100

    Betalinger + Harrys_salon

--- src/Betalinger.java
+++ src/Betalinger.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class Betalinger{
     Customer c;
     int payment;
-
     public Betalinger(Customer c, int payment){
         this.c = c;
         this.payment = payment;
     }
+
 
 
 
